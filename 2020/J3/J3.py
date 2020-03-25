3c87cc9853b05de3ada9036e7a538a3d50e1cb4f
Nirmal Jyoti Biswas <59522492+RedProgamer@users.noreply.github.com>
2020-03-25 18:48:26 +0530

    Update J3.py
    Pythonic Syntex

--- 2020/J3/J3.py
+++ 2020/J3/J3.py
@@ -1,32 +1,11 @@
-from sys import stdin
+nth = int(input())
+x_lit = []
+y_lit = []
 
-input = stdin.readline
+for _ in range(nth):
+    (x, y) = map(int, input().split(','))
+    x_lit.append(x)
+    y_lit.append(y)
 
-N = int(input())
-pointsa = []
-pointsb = []
-for i in range(N):
-    x = input()
-    x.lstrip("\n")
-    a = ""
-    b = ""
-    s = True
-    for c in x:
-        if c == ",":
-            s = False
-        if not s and c != "\n" and c != ",":
-          b += c
-        if s and c != "\n" and c != ",":
-            a +=c 
-
-    a = int(a)
-    b = int(b)
-    pointsa.append(a)
-    pointsb.append(b)
-
-
-pointsa.sort()
-pointsb.sort()
-
-print str(pointsa[0]-1) + "," + str(pointsb[0]-1)
-print str(pointsa[len(pointsa)-1]+1) + "," + str(pointsb[len(pointsb)-1]+1)
+print(f'{sorted(x_lit)[0]-1},{sorted(y_lit)[0]-1}')
+print(f'{sorted(x_lit)[-1]+1},{sorted(y_lit)[-1]+1}')
